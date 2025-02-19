9615c4100f9629cf80978440bdbd431e258e062c
Nuraayu <nurayu2718@gmail.com>
2025-02-19 11:17:27 +0700

    update

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke()
+    {
+        return 'Nama: Nur Ayu Lailatul Azizah <br> NIM: 2341720044';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PhotoController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -54,3 +61,19 @@ Route::get('/user/{name?}', function ($name='John') {
     return 'Nama saya '.$name;
 });
 
+
+Route::get('/hello', [WelcomeController::class, 'hello']);
+
+Route::get('/', HomeController::class);
+Route::get('/about', AboutController::class);
+Route::get('/articles/{id}', ArticleController::class);
+
+
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+   ]);
+   Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+   ]);
